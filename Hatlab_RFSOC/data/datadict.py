ed1fb3084559b37d663da38caa548654ef196595
mkxia57 <63606948+mkxia57@users.noreply.github.com>
2026-05-01 15:23:21 -0400

    Update datadict.py
    fixed a bug caused by comparing a numpy array to str "None"

--- Hatlab_RFSOC/data/datadict.py
+++ Hatlab_RFSOC/data/datadict.py
@@ -318,7 +318,7 @@ class DataFromQDDH5:
                 # get values of sweep axes from metadata if saved in metadata
                 ax_val = _get_eval_meta(self.datadict, ax)
                 ax_dim = len(ax_val)
-                if ax_val == "None": # sweep axis not saved in meta
+                if isinstance(ax_val, str) and ax_val == "None": # sweep axis not saved in meta
                     ax_dim = -1
                     ax_val = None
                 #     # assume the data were added in order of sweep axes, this should give us the right axes values
